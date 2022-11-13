c05fe5c95717055a863d37999c8d92c9038c18d6
Albert-Asadov <alberma@code.edu.az>
2022-11-13 21:07:40 +0400

    Home Tasks

---
+++ Program.cs
@@ -0,0 +1,107 @@
+﻿#region eded 70 den yuxari olarsa fergi vurulsun uce, ashagi olarsa fergi yazilsin
+//Console.WriteLine("eded daxil edin");
+//string numStr = Console.ReadLine();
+//int number = Convert.ToInt32(numStr);
+
+
+//int constant = 70;
+//int count = 3;
+
+//if (number > constant)
+//{
+//    number -= constant;
+//    Console.WriteLine(count *= number);
+//}
+//else
+//{
+//    Console.WriteLine(constant -= number);
+//}
+#endregion
+
+
+#region ededin 2 nin quvveti olub olmamasi
+//Console.WriteLine("choose a number");
+//string numStr = Console.ReadLine();
+//int num = Convert.ToInt32(numStr);
+//Console.WriteLine("choosen number:" + num);
+
+
+//for (int i = 1; i <= num; i *= 2)
+//{
+
+//    if (i == num)
+//    {
+
+//        Console.WriteLine("eded ikinin quvvetidir");
+
+//    }
+
+//}
+#endregion
+
+
+#region iki eded arasi
+//Console.WriteLine("first number");
+//string firstStr = Console.ReadLine();
+//int firstNumber = Convert.ToInt32(firstStr);
+//Console.WriteLine("second number");
+//string secondStr = Console.ReadLine();
+//int secondNumber = Convert.ToInt32(secondStr);
+//Console.WriteLine("iki eded arasi");
+
+
+//for (int i = firstNumber; i < secondNumber; i++)
+//{
+//    Console.WriteLine(i);
+//} 
+#endregion
+
+#region X Y Z hansi boyukdur
+//Console.WriteLine( "write X");
+//string firstStr = Console.ReadLine();
+//int numberX = Convert.ToInt32(firstStr);
+//Console.WriteLine("write Y");
+//string secondStr = Console.ReadLine();
+//int numberY = Convert.ToInt32(secondStr);
+//Console.WriteLine("write Z");
+//string thirdStr = Console.ReadLine();
+//int numberZ = Convert.ToInt32(thirdStr);
+//Console.WriteLine(" verilen hansi ede boyukdur");
+
+//if(numberX > numberY && numberX > numberZ)
+//{
+//    Console.WriteLine("X ededi boyukdur");
+//}
+//else if(numberY > numberX && numberY > numberZ)
+//{
+//    Console.WriteLine("Y ededi boyukdur");
+//}
+//else 
+// {
+//    Console.WriteLine("Z ededi boyukdur");
+//}
+#endregion
+
+#region N M ededleri verilib arasindaki tek eded sayin tapin
+
+//Console.WriteLine("M ededi daxil edin");
+//string firstStr = Console.ReadLine();
+//int numberN = Convert.ToInt32(firstStr);
+//Console.WriteLine("N ededi daxil edin");
+//string secondStr = Console.ReadLine();
+//int numberM = Convert.ToInt32(secondStr);
+//Console.WriteLine("ededlerin sayi");
+
+//int count = 0;
+
+//for ( int i = numberN; i < numberM; i++)
+//{
+//    if (i % 2 == 1)
+//    {
+//        count++;
+//        Console.WriteLine(count);
+//    }
+//}
+
+
+#endregion
